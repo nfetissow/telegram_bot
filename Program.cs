47a3bde2831a036b0c73148913b6916a359fe2c1
Maximilian Lehrbaum <maximilian.lehrbaum@googlemail.com>
2018-05-28 00:13:09 +0200

    Echo Bot with Telegram.Bot
    Written a small Echo Bot with the Telegram.Bot Library.
    Bot will output message username and answer to Chat with you have written + <Text>

--- Program.cs
+++ Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types;
 
 namespace telegram_bot
 {
     class Program
     {
+        static bool ended = false;
+        static TelegramBotClient _client = new TelegramBotClient("537933934:AAGJUyagFP4x2nhtZAD2OSqUZLPODfYFpEU");
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            _client.StartReceiving();
+            _client.OnMessage += NewMessage;
+            while(!ended)
+            {
+                continue;
+            }
+            _client.StopReceiving();
+        }
+
+        async static void NewMessage(object sender, MessageEventArgs Args)
+        {
+            if (Args.Message.Text == "stop")
+                ended = !ended;
+            Console.WriteLine(Args.Message.Chat.Username + ": " + Args.Message.Text);
+            await _client.SendTextMessageAsync(Args.Message.Chat.Id, "You wrote " + Args.Message.Text);
         }
     }
 }
